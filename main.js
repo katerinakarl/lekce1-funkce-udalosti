b3cda11485a377d55c312ff141ee4a08232056ab
Katerina Karlovska <karlovska.kat@gmail.com>
2023-01-25 08:51:34 +0100

    posledniupravy

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
-
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
  *
  * @param {int} a
  * @param {int} b
  */
+
+ function changeColor() {
+  let square = document.querySelector(".ctverecek");
+  square.style.backgroundColor="green"
+}
+
 function secti(a, b) {
   let c = a + b;
   return c;
+}
+
+function show_result(a, b) {
+  document.querySelector("#vysledek").innerHTML = secti(a,b)
 }
 
 /**
  * Upozorní uživatele při spuštění.
  */
 function upozorni() {
-  alert("Gratulace, právě jsi spustila tuto funkci!");
+alert("Gratulace, právě jsi spustila tuto funkci!");
+
+/*Změna textu ve čtverečku*/
+
+let ctverecek = document.querySelector (".ctverecek");
+ctverecek.innerHTML = "Gratulace";
+
+console.log("Gratulace, právě jsi spustila tuto funkci!")
 }
+
